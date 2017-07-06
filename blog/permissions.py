1d91316a08f1b0404c495cb42a9b99dedb433bb1
CHNicelee <1242027878@qq.com>
2017-07-06 16:04:50 +0800

    修改serializers.py

--- blog/permissions.py
+++ blog/permissions.py
@@ -1,19 +1,16 @@
 # coding=utf-8
 from rest_framework import permissions
 
-
 class IsOwnerOrReadOnly(permissions.BasePermission):
-
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.session.get('user_id') is not None
 
-    def has_object_permission(self, request, view, blog):
+    def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return blog.owner.id == request.session.get('user_id')
-
+        return obj.owner.id == request.session.get('user_id')
--- blog/serializers.py
+++ blog/serializers.py
@@ -4,21 +4,28 @@ from blog.models import *
 class BlogSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.name')
     class Meta:
-        exclude = []
         model = Blog
         fields = ('id', 'title', 'body', 'owner')
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        exclude = []
         model = User
-        field = ('id', 'username', 'name')
+        fields = ('id', 'username', 'name')
 
+#custom relational field
+class BlogListField(serializers.RelatedField):
+
+    def to_representation(self, value):
+        return 'blog_id:%d,blog_title:%s' % (value.id, value.title)
 
 class UserSerializer(serializers.ModelSerializer):
-    blog_set = serializers.PrimaryKeyRelatedField(many=True, queryset=Blog.objects.all())
+    # blog_set = serializers.PrimaryKeyRelatedField(many=True, queryset=Blog.objects.all())
+    # blog_set = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='blog-detail')
+    # blog_set = serializers.StringRelatedField(many=True)
+    # blog_set = serializers.SlugRelatedField(many=True, read_only=True, slug_field='title')
+    # blog_listing = serializers.HyperlinkedIdentityField(view_name='blog-detail')
+    blog_set = BlogListField(many=True,read_only=True)
     class Meta:
-        exclude = []
         model = User
-        field = ('id', 'username', 'blog_set')
+        fields = ('id', 'username', 'blog_set')
